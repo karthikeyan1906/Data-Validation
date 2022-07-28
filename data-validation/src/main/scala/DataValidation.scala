10762cadb3c4542889290e37881f8c6d82a822f3
Karthikeyan <karthikeyan.n@thoughtworks.com>
2022-07-28 16:34:56 +0530

    Logic update with individual field validation function with class

--- data-validation/src/main/scala/DataValidation.scala
+++ data-validation/src/main/scala/DataValidation.scala
@@ -1,18 +1,12 @@
 import scala.annotation.tailrec
+import scala.collection.mutable.ListBuffer
 import scala.io.Source
 
 object DataValidation extends App {
   val sampleData = readSampleData()
-  val constraints = sampleConstraints()
   val headerRow = sampleData.head
 
-  val results =  computeResult(constraints, sampleData.tail, headerRow)
-
-  if (results.isEmpty) println("Validation succeeded")
-  else println("Validation failed for following fields:")
-
-  results.foreach(println)
-
+ computeResult(sampleData.tail, headerRow)
 
   def readSampleData(): List[Array[String]] = {
     val src = Source.fromFile("src/assets/SampleData.csv")
@@ -23,46 +17,106 @@ object DataValidation extends App {
     sampleData
   }
 
-  def sampleConstraints(): Map[String, List[String]] = {
-    Map(
-      "Field1" -> List("text", "not longer than 20"),
-      "Field2" -> List("text"),
-      "Field3" -> List("number"),
-      "Field4" -> List("text", "mm-dd-yyyy")
-    )
-  }
-
-  def computeResult(constraints: Map[String, List[String]], rowValues: List[Array[String]], rowHeaders: Array[String]): List[String]= {
+  def computeResult(rowValues: List[Array[String]], rowHeaders: Array[String]): Unit= {
     @tailrec
-    def validateRow(rows: List[Array[String]], headers: Array[String], aggregator: List[String]): List[String] = {
-      @tailrec
-      def validateField(values: Array[String], hd: Array[String], agg: List[String]): List[String] = {
-        if (values.isEmpty) agg
-        else validateField(values.tail, hd.tail, agg ++ fieldValidator(values.head, constraints(hd.head), hd.head))
+    def validateRow(rows: List[Array[String]], header: Array[String]): Unit = {
+      def validateField(values: Array[String]): Unit = {
+
+        field1Validation(values(0), header(0))
+        field2Validation(values(1), header(1))
+        field3Validation(values(2), header(2), values(4))
+        field4Validation(values(3), header(3))
+        field5Validation(values(4), header(4))
       }
 
-      if (rows.isEmpty) aggregator
-      else validateRow(rows.tail, headers, aggregator ++ validateField(rows.head, headers, List[String]()))
+      if (rows.isEmpty) Unit
+      else {
+        validateField(rows.head)
+        validateRow(rows.tail, header)
+      }
     }
 
-    validateRow(rowValues, rowHeaders, List[String]())
+    validateRow(rowValues, rowHeaders)
   }
 
-  def fieldValidator(value: String, constraints:List[String], fieldName: String): List[String] = {
-    constraints.map {
-      case "text" =>
-        if (value.matches("^[a-zA-Z\\d-]+$".r.toString)) ""
-        else s"""$fieldName : "$value" is not a text"""
-      case "not longer than 20" =>
-        if (value.length() <= 20) ""
-        else s"""$fieldName : "$value" should not be longer than 20 characters"""
-      case "number" =>
-        if (value.matches("^\\d+$".r.toString)) ""
-        else s"""$fieldName : "$value" is not a number"""
-      case "mm-dd-yyyy" =>
-        if (value.matches("^\\d{2}-\\d{2}-\\d{4}$".r.toString)) ""
-        else s"""$fieldName : "$value" should be in "mm-dd-yyyy" format"""
-    }.filter(r => r != "")
+  def field1Validation(value: String, fieldName: String): Unit ={
+    val results = ListBuffer[Option[String]]()
+    val field1 = new DataValidation(value)
+    results += field1.shouldBeText
+    results += field1.shouldNotBeLongerThan20
+
+    field1.printConsolidatedResult(results, fieldName)
   }
+
+  def field2Validation(value: String, fieldName: String): Unit ={
+    val results = ListBuffer[Option[String]]()
+    val field2 = new DataValidation(value)
+    results += field2.shouldBeText
+
+    field2.printConsolidatedResult(results, fieldName)
+  }
+
+  def field3Validation(value: String, fieldName: String, toCompare: String): Unit ={
+    val results = ListBuffer[Option[String]]()
+    val field3 = new DataValidation(value)
+    results += field3.shouldBeNumber
+    results += field3.shouldBeLessThan(toCompare)
+
+    field3.printConsolidatedResult(results, fieldName)
+  }
+
+  def field4Validation(value: String, fieldName: String): Unit ={
+    val results = ListBuffer[Option[String]]()
+    val field4 = new DataValidation(value)
+    results += field4.shouldBeText
+    results += field4.shouldBeDate
+
+    field4.printConsolidatedResult(results, fieldName)
+  }
+
+  def field5Validation(value: String, fieldName: String): Unit ={
+    val results = ListBuffer[Option[String]]()
+    val field5 = new DataValidation(value)
+    results += field5.shouldBeNumber
+
+    field5.printConsolidatedResult(results, fieldName)
+  }
+}
+
+class DataValidation(val value: String){
+  def shouldBeText:Option[String] = {
+    if (value.matches("^[a-zA-Z\\d-]+$".r.toString)) None
+    else Some("should be text")
+  }
+
+  def shouldNotBeLongerThan20: Option[String] = {
+    if (value.length() <= 20) None
+    else Some("should not be longer than 20 chars")
+  }
+
+  def shouldBeNumber: Option[String] = {
+    if (value.forall(_.isDigit)) None
+    else Some("should be number")
+  }
+
+  def shouldBeDate: Option[String] = {
+    if (value.matches("^\\d{2}-\\d{2}-\\d{4}$".r.toString)) None
+    else Some("should be of format mm-dd-yyyy")
+  }
+
+  def shouldBeLessThan(field: String): Option[String] ={
+    val lowerValue = parseDouble(value) getOrElse Double.MaxValue
+    val higherValue = parseDouble(field) getOrElse Double.MinValue
+
+    if (lowerValue < higherValue) None
+    else Some("should be less than Field5")
+  }
+
+  def printConsolidatedResult(results: ListBuffer[Option[String]], fieldName: String): Unit = {
+    val consolidatedResult = results.filter(r => r != None).flatten.mkString(" and ")
+    if (consolidatedResult != "") println(fieldName + " " + consolidatedResult)
+  }
+
+  private def parseDouble(s: String): Option[Double] = try { Some(s.toDouble) } catch { case _ => None }
 }
 
